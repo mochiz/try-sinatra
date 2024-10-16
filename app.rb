19806f0a761025f114ee27f8407b02e83c1ed9f2
mochiz <mochi.hiro43@gmail.com>
2024-10-16 14:53:27 +0900

    エントリーポイントになるapp.rbを作成し、ルーティングの土台を作成した

---
+++ app.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+end
+
+get '/memos' do
+end
+
+get '/memos/new' do
+end
+
+get '/memos/:id' do
+end
+
+post '/memos' do
+end
+
+get '/memos/:id/edit' do
+end
+
+patch '/memos/:id' do
+end
+
+delete '/memos/:id' do
+end
+
+not_found do
+end
